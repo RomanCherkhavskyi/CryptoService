3e0f3912c708a1a19ef79ee692659a79e0b271b0
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-04-26 20:26:06 +0300

    Change code in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,8 +16,7 @@ public class Main {
         int choiceFunction;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a number about currency you need" + "\n" +
-                "1:BTC, 2:ETH, 3:DOGE, 4:LTC, 5:XRP");
+        System.out.println("Please enter a number about currency you need\n1:BTC, 2:ETH, 3:DOGE, 4:LTC, 5:XRP");
         choiceCurrency = Integer.parseInt(scanner.nextLine());
         if (!new UserDataCheck().currencyCheck(choiceCurrency)) return;
 
@@ -32,8 +31,7 @@ public class Main {
         if (!new UserDataCheck().dateCheck(finishDate)) return;
 
 
-        System.out.println("Please select what you need calculate:" + "\n" +
-                "1:min, 2:max, 3:middle, 4:normal");
+        System.out.println("Please select what you need calculate:\n1:min, 2:max, 3:middle, 4:normal");
         choiceFunction = Integer.parseInt(scanner.nextLine());
         if (!new UserDataCheck().functionCheck(choiceFunction)) return;
 

7b781fdca446dab9d42d7d47bea49452310abb3d
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-04-26 19:31:30 +0300

    Delete switch-case in UserDataCheck.java

--- src/main/java/org/example/functions/UserDataCheck.java
+++ src/main/java/org/example/functions/UserDataCheck.java
@@ -22,56 +22,19 @@ public class UserDataCheck {
     }
 
     public boolean currencyCheck(int choiceCurrency) {
-        String currencyName;
-        switch (choiceCurrency) {
-            case 1:
-                currencyName = "BTC";
-                System.out.println("You choose " + currencyName + " currency");
-                return true;
-
-            case 2:
-                currencyName = "ETH";
-                System.out.println("You choose " + currencyName + " currency");
-                return true;
-
-            case 3:
-                currencyName = "DOGE";
-                System.out.println("You choose " + currencyName + " currency");
-                return true;
-
-            case 4:
-                currencyName = "LTC";
-                System.out.println("You choose " + currencyName + " currency");
-                return true;
-
-            case 5:
-                currencyName = "XRP";
-                System.out.println("You choose " + currencyName + " currency");
-                return true;
-
-            default:
-                System.out.println("You entered non-correct number of currency: " + choiceCurrency);
-                return false;
+        if (choiceCurrency <1 || choiceCurrency >5) {
+            System.out.println("You entered non-correct number of currency");
+            return false;
         }
+        return true;
     }
 
     public boolean functionCheck(int choiceFunction) {
-        switch (choiceFunction) {
-            case 1:
-                System.out.println("You choose calculate minimal value");
-                return true;
-            case 2:
-                System.out.println("You choose calculate maximal value");
-                return true;
-            case 3:
-                System.out.println("You choose calculate middle value");
-                return true;
-            case 4:
-                System.out.println("You choose calculate normal value");
-                return true;
-            default:
-                System.out.println("You entered non-correct number of function");
-                return false;
+
+        if (choiceFunction <1 || choiceFunction >4){
+            System.out.println("You entered non-correct number of function");
+            return false;
         }
+        return true;
     }
 }

6408870c5d3da7783009a08de3b93e6c5ef687bc
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-04-26 19:39:13 +0300

    Change code in UserDataCheck.java

--- src/main/java/org/example/functions/UserDataCheck.java
+++ src/main/java/org/example/functions/UserDataCheck.java
@@ -3,21 +3,17 @@ package org.example.functions;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class UserDataCheck {
 
     public boolean dateCheck(String date) {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-        Date startDate;
         try {
-            startDate = dateFormat.parse(date);
+            dateFormat.parse(date);
         } catch (ParseException e) {
             System.out.println("You enter date in non-correct date format");
             return false;
-
         }
-        System.out.println("Date is: " + startDate);
         return true;
     }
 
